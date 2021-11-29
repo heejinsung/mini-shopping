2760d20e65c9d0e52bc49682d5c69e6dc7c0c16e
heejinsung <shj683706@gmail.com>
2021-11-29 22:59:28 +0900

    버튼 이벤트 추가

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # mini-shopping
-
-git test
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-// Fetch the items from the JSON file
+// Fetch the items from the JSON file (fetch: 동적으로 받아오기)
 function loadItems() {
   return fetch('data/data.json')
     .then(response => response.json())
@@ -22,10 +22,33 @@ function createHTMLString(item) {
   `
 }
 
+// Handle button click
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+  
+  if (key == null || value == null) {
+    return;
+  }
+
+  // const filtered = items.filter(item => item[key] === value);
+  // console.log(filtered)
+  displayItems(items.filter(item => item[key] === value));
+  
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', () => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then(items => {
     displayItems(items);
-    // setEventListeners(items)
+    setEventListeners(items)
   })
   .catch(console.log)
